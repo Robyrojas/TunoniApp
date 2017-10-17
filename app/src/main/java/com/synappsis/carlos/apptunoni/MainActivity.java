c45eb0efb5eec9d49d3889c32ee94bf3d11f4cb6
Robyrojas <c.armando.diaz.p@gmail.com>
2017-10-17 12:42:47 -0500

    Nav Drawer

--- app/src/main/java/com/synappsis/carlos/apptunoni/MainActivity.java
+++ app/src/main/java/com/synappsis/carlos/apptunoni/MainActivity.java
@@ -1,7 +1,10 @@
 package com.synappsis.carlos.apptunoni;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.initSesion);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Nav_Principal.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
