9940a686da09b72867da5e43bef6e5cecbe52cb6
Robyrojas <c.armando.diaz.p@gmail.com>
2017-10-24 16:33:50 -0500

    Nav Drawer 5 desing

--- app/src/main/java/com/synappsis/carlos/apptunoni/Nav_Principal.java
+++ app/src/main/java/com/synappsis/carlos/apptunoni/Nav_Principal.java
@@ -40,14 +40,6 @@ public class Nav_Principal extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         Fragment fragmento = new EntregaProceso();
         getSupportFragmentManager().beginTransaction().replace(R.id.Contenedor,fragmento).commit();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
